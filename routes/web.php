bebc76cd6d01e616cdf947c2809f0a0550fdc5c8
csw <zobirofkir19@gmail.com>
2025-05-19 17:14:55 +0100

    fix error from name

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
+    return inertia('WelcomePage');
 })->name('home');
 
 
