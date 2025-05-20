5f2247566704e052b052df1a44d87c2ed51e3d5e
csw <zobirofkir19@gmail.com>
2025-05-20 14:25:26 +0100

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,11 +30,11 @@
             }
         </style>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Alem') }}</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        {{-- <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml"> --}}
+        {{-- <link rel="apple-touch-icon" href="/apple-touch-icon.png"> --}}
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
