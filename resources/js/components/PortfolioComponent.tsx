1d382f10333773c42f35db7d7f76507eac0da681
csw <zobirofkir19@gmail.com>
2025-05-20 11:56:27 +0100

    create portfolio data

--- resources/js/components/PortfolioComponent.tsx
+++ resources/js/components/PortfolioComponent.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { cn } from '../lib/utils';
+import { portfolioItems } from '@/data/PortfolioData';
 
 interface PortfolioItemProps {
   title: string;
@@ -44,40 +45,7 @@ const PortfolioItem: React.FC<PortfolioItemProps> = ({ title, category, image, d
 
 const PortfolioComponent: React.FC = () => {
   const [activeFilter, setActiveFilter] = useState('Tous');
-  
-  const portfolioItems = [
-    {
-      title: "Biologie de la reproduction",
-      category: "Infertilité",
-      image: "https://images.unsplash.com/photo-1579154204601-01588f351e67?q=80&w=1470"
-    },
-    {
-      title: "Analyses médicales rapides",
-      category: "Laboratoire",
-      image: "https://images.unsplash.com/photo-1582719471384-894fbb16e074?q=80&w=1470"
-    },
-    {
-      title: "Prélèvement à domicile",
-      category: "Services",
-      image: "https://images.unsplash.com/photo-1584982751601-97dcc096659c?q=80&w=1472"
-    },
-    {
-      title: "Équipement de pointe",
-      category: "Technologie",
-      image: "https://images.unsplash.com/photo-1666214277657-e60f05c40b04?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
-      title: "Radio-biologie",
-      category: "Spécialité",
-      image: "https://images.unsplash.com/photo-1530026405186-ed1f139313f8?q=80&w=1470"
-    },
-    {
-      title: "Résultats fiables",
-      category: "Qualité",
-      image: "https://images.unsplash.com/photo-1576086213369-97a306d36557?q=80&w=1480"
-    }
-  ];
-  
+    
   const filters = ['Tous', 'Laboratoire', 'Infertilité', 'Services', 'Technologie', 'Spécialité', 'Qualité'];
   
   const filteredItems = activeFilter === 'Tous' 
