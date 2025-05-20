18d736ce31e630af38eaa5b69c8d4e3f34fe2cdb
csw <zobirofkir19@gmail.com>
2025-05-20 16:24:18 +0100

    create contact model and migration

---
+++ app/Models/Contact.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Contact extends Model
+{
+    use HasFactory;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'subject',
+        'message',
+        'status',
+    ];
+}
