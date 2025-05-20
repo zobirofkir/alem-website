c222cd01448218d4a141fbc6f70c6629c2540fe6
csw <zobirofkir19@gmail.com>
2025-05-20 10:35:59 +0100

    create reproduction page

--- resources/js/pages/ReproductionPage.tsx
+++ resources/js/pages/ReproductionPage.tsx
@@ -3,7 +3,7 @@ import ReproductionComponent from '@/components/ReproductionComponent'
 const ReproductionPage = () => {
   return (
     <section className='dark:bg-black bg-white'>
-        <div className='flex justify-center items-center lg:h-screen'>
+        <div className='flex justify-center items-center'>
             <ReproductionComponent />
         </div>
     </section>
