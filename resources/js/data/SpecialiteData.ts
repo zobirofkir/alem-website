0b9f2d45fb77265f00584feeaf5402d1a33f6895
csw <zobirofkir19@gmail.com>
2025-06-16 15:10:16 +0100

    update speciality data

--- resources/js/data/SpecialiteData.ts
+++ resources/js/data/SpecialiteData.ts
@@ -2,19 +2,28 @@
  * Specialite Data
  */
 export const specialites = [
-    {
-      icon: "👶",
-      title: "Biologie de la reproduction - Infertilité",
-      description: "Accompagnement des couples\nSuivi hormonal, spermogrammes, bilans pré-conceptionnels"
-    },
-    {
-      icon: "🧪",
-      title: "Analyses médicales courantes",
-      description: "Bilan sanguin, analyses urinaires, tests viraux\nRésultats rapides, condentialité garantie"
-    },
-    {
-      icon: "☢️",
-      title: "Radio-biologie",
-      description: "Analyses liées à l'exposition aux rayonnements\nSuivi spécique dans certains traitements"
-    }
-  ];
+  {
+    icon: "👶",
+    title: "Biologie de la reproduction - Infertilité",
+    description:
+      "Prise en charge des couples en parcours de fertilité\nSuivi hormonal, spermogrammes, bilans pré-conceptionnels personnalisés",
+  },
+  {
+    icon: "🧪",
+    title: "Analyses médicales rapides & fiables",
+    description:
+      "Bilan sanguin, analyses urinaires, tests viraux\nRésultats rapides avec confidentialité garantie",
+  },
+  {
+    icon: "☢️",
+    title: "Radio-biologie",
+    description:
+      "Analyses spécialisées liées à l’exposition aux rayonnements\nSuivi dans les traitements nécessitant une surveillance radiobiologique",
+  },
+  {
+    icon: "🏡",
+    title: "Prélèvement à domicile",
+    description:
+      "Service pratique pour les patients à mobilité réduite ou occupés\nIntervention rapide à domicile sur rendez-vous",
+  },
+];
