891c796fd7ec3b7dea8836d5c721c3b5e4022ef2
csw <zobirofkir19@gmail.com>
2025-06-16 14:48:22 +0100

    update portfolio component

--- resources/js/components/PortfolioComponent.tsx
+++ resources/js/components/PortfolioComponent.tsx
@@ -77,7 +77,6 @@ const PortfolioComponent: React.FC = () => {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {filteredItems.map((item, index) => {
-            // Find the original index in the portfolioItems array
             const originalIndex = portfolioItems.findIndex(
               portfolioItem => portfolioItem.title === item.title && portfolioItem.category === item.category
             );
